b521a01a00dec771f84c08f3844b2cc7db48906a
hendesprit <hend.zormati@esprit.tn>
2024-10-13 17:13:21 +0100

    update / delete project page

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -23,7 +23,7 @@ class ProjectsController extends AbstractController
     #[Route('/projects/{cat}/{page}', name: 'app_projects')]
     public function index(int $cat ,int $page,Request $request, InvestmentsRepository $invrepo,CategoryRepository $catrepo ): Response
     {
-        $itemsPerPage = 2; // Number of items per page
+        $itemsPerPage = 4; // Number of items per page
         $Cat= $catrepo->find($cat);
        if ( $Cat == null ) { $totalItems = count($invrepo->findAllSortedByFundingDifference()); // Total number of items
         $totalPages = ceil($totalItems / $itemsPerPage); // Calculate the total number of pages
@@ -65,20 +65,58 @@ class ProjectsController extends AbstractController
         ]);
     }
     #[Route('/infos/{id}', name: 'app_project_edit')]
-    public function editproject(int $id, InvestmentsRepository $invrepo,CategoryRepository $catrepo): Response
+    public function editproject(int $id,Request $request,SluggerInterface $slugger, ParameterBagInterface $params, EntityManagerInterface $entityManager,InvestmentsRepository $invrepo,CategoryRepository $catrepo): Response
     {
         $categories= $catrepo->findAll();
         $reccprojets= $invrepo->findAllSortedByFundingDifference();
+
         $projet= $invrepo->find($id);
-        return $this->render('home/details.html.twig', [
+        $form = $this->createForm(InvestmentType::class, $projet);
+        $categories= $catrepo->findAll();
+        $reccprojets= $invrepo->findAllSortedByFundingDifference();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $projet->setUpdatedAt(new \DateTimeImmutable());
+            $imageFile = $form->get('imageFile')->getData(); // Ensure 'imageFile' matches your form field name
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = substr($safeFilename, 0, 10).'-'.uniqid().'.'.$imageFile->guessExtension();
+    
+                try {
+                    $imageFile->move(
+                        $params->get('projet_pictures_directory'), // Make sure this parameter is defined in your services.yaml
+                        $newFilename
+                    );
+                    $projet->setImage($newFilename); // Update the entity with the new filename
+                } catch (FileException $e) {
+                    // Handle exception if something happens during file upload
+                }
+            }
+            $entityManager->flush();
+
+            return $this->redirectToRoute('project_detail', ['id'=> $projet->getId()], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('home/updateprojet.html.twig', [
             'controller_name' => 'HomeController',
             'part' => 3,
             'title' => $projet->getName(),
-            'titlepage' => 'Détails - ',
+            'titlepage' => 'Projet - ',
             'projet'=>$projet,
             'categories'=>$categories,
             'reccprojets' =>$reccprojets,
+            'form' => $form->createView(),
         ]);
+    }
+    #[Route('/projet/{id}', name: 'app_projet_delete', methods: ['POST'])]
+    public function deleteproject(Request $request,  int $id, EntityManagerInterface $entityManager,InvestmentsRepository $invrepo): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$id, $request->request->get('_token'))) {
+            $entityManager->remove($invrepo->find($id));
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_projects', ['cat'=> 0,'page'=>1], Response::HTTP_SEE_OTHER);
     }
     #[Route('/projet', name: 'add_project')]
     public function addproject(Request $request,SluggerInterface $slugger, ParameterBagInterface $params, EntityManagerInterface $entityManager,InvestmentsRepository $invrepo,CategoryRepository $catrepo): Response
@@ -125,6 +163,7 @@ class ProjectsController extends AbstractController
             'reccprojets' =>$reccprojets,
             'projet'=>$projet,
             'form' => $form->createView(),
+            
         ]);
     }
 }
